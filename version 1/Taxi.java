10c4f66cb1f5d5527acbac2f676f982e27544cb9
ThomasC99 <ThomasC99@users.noreply.github.com>
2020-08-31 15:20:46 -0400

    Create Taxi.java

---
+++ version 1/Taxi.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Taxi {
+	static String location;
+	static String job;
+	static double money;
+	static int sign;
+	static void chicago () {
+	}
+	static void jobMenu () {
+	}
+	static void losAngeles () {
+	}
+	public static void main (String [] args) {
+		location = "chicago";
+		job = "";
+		money = 0;
+		sign = 0;
+		System.out.println("");
+		System.out.println("");
+		mainMenu();
+	}
+	static void mainMenu () {
+	}
+	static void mexicoCity () {
+	}
+	static void newYork () {
+	}
+	static void upgrades () {
+	}
+}
