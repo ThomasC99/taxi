817acd35cde584cbca1ec0882163dbee1eca723a
ThomasC99 <ThomasC99@users.noreply.github.com>
2021-01-07 11:31:45 -0500

    Update Player.java

---
+++ version 1/command-line/Player.java
@@ -0,0 +1,12 @@
+class Player {
+	private String job;
+	public Player () {
+		this.job = "";
+	}
+	public String getJob () {
+		return job;
+	}
+	public void setJob (String job) {
+		this.job = job;
+	}
+} // 11
