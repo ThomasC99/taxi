045a0f9840a051797bc2e5bd682813b265cb88b3
ThomasC99 <ThomasC99@users.noreply.github.com>
2021-01-07 20:01:04 -0500

    Update Player.java

--- version 1/command-line/Player.java
+++ version 1/command-line/Player.java
@@ -1,3 +1,4 @@
+import java.io.File;
 class Player {
 	private String location;
 	private String job;
@@ -11,7 +12,52 @@ class Player {
 		this.sign = 10;
 		this.gps = 1;
 	}
+	public void load () {
+		try {
+			File file = new File ("save.txt");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+		}
+	}
+	public void save () {
+		try {
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("");
+			System.out.println("");
+		}
+	}
 	public String getLocation () {
 		return location;
+	}
+	public String getJob () {
+		return job;
+	}
+	public double getMoney () {
+		return money;
+	}
+	public int getSign () {
+		return sign;
+	}
+	public int getGps () {
+		return gps;
+	}
+	public void setLocation (String location) {
+		this.location = location;
+	}
+	public void setJob (String job) {
+		this.job = job;
+	}
+	public void setMoney (double money) {
+		this.money = money;
+	}
+	public void setSign (int sign) {
+		this.sign = sign;
+	}
+	public void setGps (int gps) {
+		this.gps = gps;
 	}
 }
