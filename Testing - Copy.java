ae48272a9881ce3989b67563bd3ddd43d19c3c4f
ThomasC99 <ThomasC99@users.noreply.github.com>
2021-04-22 00:39:06 -0400

    Create Testing - Copy.java

---
+++ Testing - Copy.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Random;
+class Testing {
+	public static void main (String [] args) throws Exception {
+		Random random = new Random (0);
+		File file = new File ("testing.txt");
+		file.delete();
+		file.createNewFile();
+		FileWriter writer = new FileWriter (file);
+		for (byte i = 0; i < Byte.MAX_VALUE; i++) {
+			writer.write((random.nextInt(4) + 1) + "\n");
+		}
+		writer.close();
+	}
+}
